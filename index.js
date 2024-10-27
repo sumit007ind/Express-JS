0d1f15990f27383289d5ad75deb7cb9e97802885
sumit007ind <sharmaraj81808@gmail.com>
2024-10-27 21:49:41 +0530

    created requests

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Sumit sharma!')
 })
 
+app.get('/', (req,res) => {
+    res.send('getting responce!!');
+})
+ 
+app.put('items' , (req,res) => {
+    res.send('putting responce');
+})
+
 app.listen(port, () => {
   console.log(`Sumit Shrama's app listening on port ${port}`)
 })
