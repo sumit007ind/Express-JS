86a4c587d4af67bb8bbb028a3ff20f406f883010
sumit007ind <sharmaraj81808@gmail.com>
2024-10-28 21:31:10 +0530

    ...

--- routes/items.js
+++ routes/items.js
@@ -4,22 +4,22 @@ const router = express.Router()
 
 
 // define the home page route
-app.get('/', (req, res) => {
-    // res.send('got a git request!')
-    res.sendFile('./dummy.html' , {root:__dirname})
+router.get('/', (req, res) => {
+     res.send('got a git request!')
+    // res.sendFile('../dummy.html' , {root:__dirname})
   })
   
   
-  app.post('/items' , (req,res) => {
+  router.post('/items' , (req,res) => {
       // res.send('putting responce');
       res.json({x:1,y:2,z:3})
   })
   
-  app.put('/items/:id' , (req,res) => {
+  router.put('/items/:id' , (req,res) => {
       res.send(' responce');
   })
   
-  app.delete('/items/:id' , (req,res) => {
+  router.delete('/items/:id' , (req,res) => {
       res.send('Deleting responce');
   })
   
