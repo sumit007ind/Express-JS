4f4621db3a233fb0b4e0193cb66dfa77cf1a2bf4
sumit007ind <sharmaraj81808@gmail.com>
2024-10-28 20:20:02 +0530

    .....

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('got a git request!')
+  // res.send('got a git request!')
+  res.sendFile('./dummy.html' , {root:__dirname})
 })
 
 
